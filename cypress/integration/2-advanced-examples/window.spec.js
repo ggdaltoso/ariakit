e7d1514643045dc6599f68952a9ca49ec0a1f245
Haz <hazdiego@gmail.com>
2022-03-29 16:40:35 +0200

    Trying cypress

---
+++ cypress/integration/2-advanced-examples/window.spec.js
@@ -0,0 +1,22 @@
+/// <reference types="cypress" />
+
+context("Window", () => {
+  beforeEach(() => {
+    cy.visit("https://example.cypress.io/commands/window");
+  });
+
+  test("cy.window() - get the global window object", () => {
+    // https://on.cypress.io/window
+    cy.window().should("have.property", "top");
+  });
+
+  test("cy.document() - get the document object", () => {
+    // https://on.cypress.io/document
+    cy.document().should("have.property", "charset").and("eq", "UTF-8");
+  });
+
+  test("cy.title() - get the title", () => {
+    // https://on.cypress.io/title
+    cy.title().should("include", "Kitchen Sink");
+  });
+});
---
+++ packages/ariakit-playground/src/__examples__/playground/e2e.ts
@@ -0,0 +1,7 @@
+/// <reference types="cypress" />
+
+describe("lol", () => {
+  test("lol", () => {
+    cy.visit("http://localhost:3000/examples/playground");
+  });
+});
